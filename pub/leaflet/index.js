a5c4f221a32f88251473976c66031533e571b4a1
tingold@boundlessgeo.com <tingold@boundlessgeo.com>
2017-05-16 09:11:21 -0500

    removed old demo

--- pub/leaflet/index.js
+++
@@ -1,6 +0,0 @@
-//var url = '/geowebcache/service/gmaps?layers=planet&zoom={z}&x={x}&y={y}&FORMAT=application/x-protobuf;type=mapbox-vector';
-var url ='/tile/{z}/{x}/{y}'
-var b = L.vectorGrid.protobuf(url);
-
-var map = L.map('map').setView([0, 0], 2);
-map.addLayer(b);
